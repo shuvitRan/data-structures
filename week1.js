786733b218e618ab7b53fbb5b053648a0e00237a
EC2 Default User <ec2-user@ip-172-31-8-77.us-east-2.compute.internal>
2018-08-28 01:37:21 +0000

    first commit of week1 assignment

---
+++ week1.js
@@ -0,0 +1,13 @@
+
+// npm install request
+// mkdir data
+
+var request = require('request');
+var fs = require('fs');
+
+request('https://parsons.nyc/thesis-2018/', function(error, response, body){
+    if (!error && response.statusCode == 200) {
+        fs.writeFileSync('/home/ec2-user/environment/data/thesis.txt', body);
+    }
+    else {console.log("Request failed!")}
+});
