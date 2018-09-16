20f5da326bc91d5e35bb92acd48833d02e294979
Dan Ran <randanfx@gmail.com>
2018-09-16 04:08:42 +0000

    reorgnized folder add Readme folder

---
+++ README.md
@@ -0,0 +1,3 @@
+Data-Structures 
+
+Dan Ran
--- Week1/week1.js
+++ Week1/week1.js
@@ -63,29 +63,29 @@ function PageNumber(Number){
 function DelayLoop(){
     
     
-   //request the web pages with delay. waiting for each page go though      
+   //request the Ten pages with delay. waiting for each page go though      
  setTimeout(
       function () {    
              
                         
       if (i < 11) {   
-          Hnumber=PageNumber(i);
+            Hnumber=PageNumber(i);
              i++; 
              
-    //call it self again with i+1         
-         DelayLoop();             
-      console.log(Hnumber);
-      
-          request('https://parsons.nyc/aa/m'+ Hnumber +'.html', function(error, response, body){
-    if (!error && response.statusCode == 200) {
-        fs.writeFileSync('/home/ec2-user/environment/data/m'+ Hnumber +'.txt', body);
-    }
-    else {console.log("Request failed!")}
-   
-});
+            //call it self again with i+1         
+            DelayLoop();             
+            console.log(Hnumber);
+              
+            request('https://parsons.nyc/aa/m'+ Hnumber +'.html', function(error, response, body){
+            if (!error && response.statusCode == 200) {
+                fs.writeFileSync('/home/ec2-user/environment/Week1/data/m'+ Hnumber +'.txt', body);
+            }
+            else {console.log("Request failed!")}
+           
+            });
          
       }                     
-    }, 200) //delay 0.2sec
+    }, 200) 
 }
         
 DelayLoop();
