4794ad70d70325c484dd14557dac764e3655f021
Dan Ran <randanfx@gmail.com>
2018-09-18 01:41:46 +0000

    Week 1 different solution, and start of week 2

---
+++ Week2/Week2.js
@@ -0,0 +1,25 @@
+// npm install cheerio
+
+var fs = require('fs');
+var cheerio = require('cheerio');
+
+// load the thesis text file into a variable, `content`
+// this is the file that we created in the starter code from last week
+var content = fs.readFileSync('data/thesis.txt');
+
+// load `content` into a cheerio object
+var $ = cheerio.load(content);
+
+// print (to the console) names of thesis students
+$('h3').each(function(i, elem) {
+    console.log($(elem).text());
+});
+
+// write the project titles to a text file
+var thesisTitles = ''; // this variable will hold the lines of text
+
+$('.project .title').each(function(i, elem) {
+    thesisTitles += ($(elem).text()) + '\n';
+});
+
+fs.writeFileSync('data/thesisTitles.txt', thesisTitles);
