234cf1f685b08d142475c1ddb3ea46102badbb4b
Dan Ran <randanfx@gmail.com>
2018-11-27 01:46:15 +0000

    description added

--- Week2/data/Test.js
+++
@@ -1,7 +0,0 @@
-var fs=require('fs');
-
-var content = fs.readFileSync("AAInfo.json");
-
-var data=JSON.parse(content);
-
-console.log(data);
--- Week3/testCleanGeo.js
+++
@@ -1,9 +0,0 @@
-var fs=require('fs');
-
-var content= fs.readFileSync("data/M10CleanData.json");
-var data=JSON.parse(content);
-
-console.log(data.length);
-
-console.log(data);
-
